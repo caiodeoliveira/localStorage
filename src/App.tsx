b99eb21ee7cd811ffe4753650624ea0018e711aa
Caio Ribeiro <Caio.ribeiro98@outlook.com>
2022-03-05 11:23:44 -0300

    🏗️  Changes in Structure of Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import "./styles.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Login } from "./components/Login";
+import { Home } from "./components/Home";
+
 function App() {
-  return <Login />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
