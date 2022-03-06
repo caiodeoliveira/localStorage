f95fd448ea3021869f829c31344248c295ca4a6c
Caio Ribeiro <Caio.ribeiro98@outlook.com>
2022-03-06 15:57:51 -0300

    🚧 Add Register page and install styled components

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/" element={<Login />} />
         <Route path="/home" element={<Home />} />
       </Routes>
     </BrowserRouter>
---
+++ src/components/Register/index.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import * as S from "./styles";
+
+const Register = () => {
+  return <S.Container></S.Container>;
+};
